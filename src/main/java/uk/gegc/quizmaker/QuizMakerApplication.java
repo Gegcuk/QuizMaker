040dd53794e538b9c3cff96f2329d83fd06369e6
Aleksei Lazunin <gegcuk@gmail.com>
2025-04-14 01:14:06 +0100

    Template for User and Roles entities

--- src/main/java/uk/gegc/quizmaker/QuizMakerApplication.java
+++ src/main/java/uk/gegc/quizmaker/QuizMakerApplication.java
@@ -1,13 +1,18 @@
 package uk.gegc.quizmaker;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @SpringBootApplication
 public class QuizMakerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QuizMakerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(QuizMakerApplication.class, args);
+    }
+
 
 }
--- src/main/java/uk/gegc/quizmaker/model/userManagment/User.java
+++ src/main/java/uk/gegc/quizmaker/model/userManagment/User.java
@@ -1,7 +1,12 @@
-package uk.gegc.quizmaker.model;
+package uk.gegc.quizmaker.model.userManagment;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -14,6 +19,31 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", updatable = false)
     private Long userId;
+
+    @NotBlank
+    @Size(min = 4, message = "Username has to be longer than 4 symbols")
+    @Size(max = 20, message = "Username has to be shorter than 20 symbols")
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+
+    @NotBlank
+    @Email
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String hashedPassword;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private List<Role> roles;
+
+
 
 }
