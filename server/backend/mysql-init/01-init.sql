fcb5cd95fbb49e954a94766bd89a66f1351445a4
Aleksei Lazunin <gegcuk@gmail.com>
2025-09-28 03:53:47 +0100

    Add debug messages to MySQL init script to verify environment variables and user permissions

--- server/backend/mysql-init/01-init.sql
+++ server/backend/mysql-init/01-init.sql
@@ -1,6 +1,12 @@
 -- Initialize QuizMaker database
 -- This script runs when MySQL container starts for the first time
 
+-- Debug: Show environment variables
+SELECT 'DEBUG: MySQL init script started' as debug_message;
+SELECT CONCAT('DEBUG: MYSQL_USER = ', IFNULL(@MYSQL_USER, 'NULL')) as debug_message;
+SELECT CONCAT('DEBUG: MYSQL_PASSWORD = ', IFNULL(@MYSQL_PASSWORD, 'NULL')) as debug_message;
+SELECT CONCAT('DEBUG: MYSQL_DATABASE = ', IFNULL(@MYSQL_DATABASE, 'NULL')) as debug_message;
+
 -- Create database if it doesn't exist (should already be created by MYSQL_DATABASE env var)
 CREATE DATABASE IF NOT EXISTS quizmakerdb CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -8,13 +14,24 @@ CREATE DATABASE IF NOT EXISTS quizmakerdb CHARACTER SET utf8mb4 COLLATE utf8mb4_
 -- using MYSQL_USER and MYSQL_PASSWORD environment variables
 -- But we need to ensure the user can connect from any host in Docker network
 -- The user is created by MySQL entrypoint, we just need to grant privileges from any host
--- We'll use a dynamic approach to get the actual username from environment
-SET @user_name = (SELECT SUBSTRING_INDEX(USER(), '@', 1));
-SET @sql = CONCAT('GRANT ALL PRIVILEGES ON quizmakerdb.* TO ''', @user_name, '''@''%''');
-PREPARE stmt FROM @sql;
-EXECUTE stmt;
-DEALLOCATE PREPARE stmt;
+-- We'll use the actual username from the environment variable
+
+-- Debug: Show existing users before granting privileges
+SELECT 'DEBUG: Existing users before grant:' as debug_message;
+SELECT User, Host FROM mysql.user WHERE User = '${MYSQL_USER}';
+
+-- Grant privileges from any host
+SELECT CONCAT('DEBUG: Granting privileges to user: ', '${MYSQL_USER}') as debug_message;
+GRANT ALL PRIVILEGES ON quizmakerdb.* TO '${MYSQL_USER}'@'%';
 FLUSH PRIVILEGES;
+
+-- Debug: Show users after granting privileges
+SELECT 'DEBUG: Users after grant:' as debug_message;
+SELECT User, Host FROM mysql.user WHERE User = '${MYSQL_USER}';
+
+-- Debug: Show privileges for the user
+SELECT 'DEBUG: Privileges for user:' as debug_message;
+SHOW GRANTS FOR '${MYSQL_USER}'@'%';
 
 -- Set timezone
 SET GLOBAL time_zone = '+00:00';
