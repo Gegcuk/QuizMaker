424cf44062c3b464cbf071243c11925540054bdb
Aleksei Lazunin <gegcuk@gmail.com>
2025-09-28 03:42:27 +0100

    Fix MySQL user permissions: use DB_USERNAME from secrets and grant privileges from any host

--- server/backend/mysql-init/01-init.sql
+++ server/backend/mysql-init/01-init.sql
@@ -6,6 +6,15 @@ CREATE DATABASE IF NOT EXISTS quizmakerdb CHARACTER SET utf8mb4 COLLATE utf8mb4_
 
 -- User creation and privileges are automatically handled by MySQL entrypoint
 -- using MYSQL_USER and MYSQL_PASSWORD environment variables
+-- But we need to ensure the user can connect from any host in Docker network
+-- The user is created by MySQL entrypoint, we just need to grant privileges from any host
+-- We'll use a dynamic approach to get the actual username from environment
+SET @user_name = (SELECT SUBSTRING_INDEX(USER(), '@', 1));
+SET @sql = CONCAT('GRANT ALL PRIVILEGES ON quizmakerdb.* TO ''', @user_name, '''@''%''');
+PREPARE stmt FROM @sql;
+EXECUTE stmt;
+DEALLOCATE PREPARE stmt;
+FLUSH PRIVILEGES;
 
 -- Set timezone
 SET GLOBAL time_zone = '+00:00';
